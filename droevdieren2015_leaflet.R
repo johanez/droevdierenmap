bb70371b748eae05ba0467c2326a86ff5ec26a28
Johannes Eberens <ebere001@papaya.(none)>
2015-07-17 11:00:02 +0200

    leafletmap html

--- droevdieren2015_leaflet.R
+++ droevdieren2015_leaflet.R
@@ -1,10 +1,18 @@
 require(leaflet)
+require(dplyr)
+d1 <- read.csv("dierentellingspa.csv", stringsAsFactors = F)
+
+cols <- brewer.pal(10, name = 'Set3')
+
 d1$name[is.na(d1$name)] <- "nameless"
+d1$extra[is.na(d1$extra)] <- ""
+d1$mainspecies <- as.factor(d1$mainspecies)
+
 leaflet(d1) %>%addProviderTiles("Stamen.Toner") %>% 
   addCircleMarkers(lng = ~jitterlong, lat=~jitterlat, 
              fillColor = ~mscolor, color=~mscolor,
              radius=10, weight=1, fillOpacity=0.6,
-             popup = paste0(d1$name, " ", d1$species, ", " ,d1$extra)) %>%
+             popup = paste0(d1$name, "<br>", d1$species, "<br>" ,d1$extra)) %>%
   addLegend(pal = colorFactor(palette = cols, levels = 1:10),
             values = ~mainspecies, opacity = 1, title="Species")
 
